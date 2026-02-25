fa2bb242704848382e1b1e6ec69fccfba0598576
olimsaidov <olim.mail@gmail.com>
2026-02-25 06:35:20 +0000

    Deploying to gh-pages from @ HealthSamurai/formbox-renderer@90c09c1f2953bc4b50cd539d05878ca700220323 🚀

--- server/server.js
+++ server/server.js
@@ -35,11 +35,11 @@ const renderNotFound = async (title) => {
 };
 const loadDocumentation = async () => {
   const [manifest, layout, documentPage, documentationIndex, themes] = await Promise.all([
-    import("./assets/manifest-C-7eEzzR.js").then((n) => n.m),
-    import("./assets/layout-BqZVv04q.js"),
-    import("./assets/page-d0Mpuyt4.js"),
+    import("./assets/manifest-NUSWq3xB.js").then((n) => n.m),
+    import("./assets/layout-BRcQfUxm.js"),
+    import("./assets/page-CzRklbum.js"),
     import("./assets/index-Biw2Xbl-.js"),
-    import("./assets/themes-ls7ostG8.js")
+    import("./assets/themes-5B3HRTRu.js")
   ]);
   return {
     routes: manifest.routes,
@@ -52,7 +52,7 @@ const loadDocumentation = async () => {
 };
 const getRoutes = async () => {
   {
-    const { routes } = await import("./assets/manifest-C-7eEzzR.js").then((n) => n.m);
+    const { routes } = await import("./assets/manifest-NUSWq3xB.js").then((n) => n.m);
     return ["/", "/swm/", "/docs/", "/docs/themes/", ...routes.keys()];
   }
 };
@@ -136,7 +136,7 @@ const resolveRoute = async (url) => {
   if (pathname !== "/") {
     return await renderNotFound("Not Found");
   }
-  const { default: Landing } = await import("./assets/index-tgnjz-0i.js");
+  const { default: Landing } = await import("./assets/index-0sXzuOMf.js");
   return {
     element: /* @__PURE__ */ jsx(Landing, {}),
     title: "Formbox Renderer"
--- storybook/assets/index-CTmOJry4.js
+++
@@ -1,1 +0,0 @@
-import{M as r}from"./iframe-BAyVixai.js";import{r as o}from"./index-OSEiohvb.js";var t=o();const m=r(t);export{m as R,t as r};
